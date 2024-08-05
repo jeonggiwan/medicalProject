f0708c4d2f9867394a452908f585ffeabdae3ed3
Jeong Giwan <rldhks1010@naver.com>
2024-08-05 12:44:18 +0900

    Token Error

--- src/main/java/com/springbook/biz/security/JwtAuthenticationFilter.java
+++ src/main/java/com/springbook/biz/security/JwtAuthenticationFilter.java
@@ -1,13 +1,13 @@
 package com.springbook.biz.security;
 
 import java.io.IOException;
-import java.util.Collections;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -24,16 +24,33 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
-        String token = jwtTokenProvider.resolveToken(httpRequest);
-        System.out.println("Token: " + token);
-        System.out.println("All Headers:");
-        Collections.list(httpRequest.getHeaderNames()).forEach(headerName -> 
-            System.out.println(headerName + ": " + httpRequest.getHeader(headerName))
-        );
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            Authentication auth = jwtTokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(auth);
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        String token = jwtTokenProvider.resolveRefreshToken(httpRequest);
+        if (token == null) {
+            token = httpRequest.getHeader("Authorization");
         }
+        
+        if (token != null) {
+            if (jwtTokenProvider.validateToken(token)) {
+                Authentication auth = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            } else {
+                // 액세스 토큰이 만료된 경우
+                String refreshToken = jwtTokenProvider.resolveRefreshToken(httpRequest);
+         
+                if (refreshToken != null && jwtTokenProvider.validateToken(refreshToken)) {
+                    String username = jwtTokenProvider.getUserPk(refreshToken);
+                    String newAccessToken = jwtTokenProvider.createAccessToken(username);
+                    httpResponse.setHeader("Authorization", "Bearer " + newAccessToken);
+                    Authentication auth = jwtTokenProvider.getAuthentication(newAccessToken);
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                    
+                    System.out.println("New access token created: " + newAccessToken);
+                }
+            }
+        }
+        
         filterChain.doFilter(servletRequest, servletResponse);
     }
 }
